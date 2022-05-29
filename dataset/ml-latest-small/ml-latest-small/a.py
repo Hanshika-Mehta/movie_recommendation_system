dc1da845d5fb4d0ac5b94c99bd956ff4e6ed9206
Hanshika <hanshika.mehta15@gmail.com>
2022-05-29 20:12:22 +0530

    data remove

--- dataset/ml-latest-small/ml-latest-small/a.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-# df = pd.read_csv("movies.csv")
-
-def movies(request):
-    df = pd.read_csv('movies.csv')
-    for movieId, title, genres in zip(df.movieId, df.title, df.genres):
-    models = movies(movieid = movieId, title=title, genres=genres)
-    models.save()
-    return redirect("loginapp:about")
